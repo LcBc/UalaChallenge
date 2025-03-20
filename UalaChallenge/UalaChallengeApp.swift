e7a758ee12f002361ee2739a04d9a7afe64abcc1
luis barrios <luis.barrios@externos.bci.cl>
2025-03-20 17:10:31 -0300

    Initial Commit

---
+++ UalaChallenge/UalaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaChallengeApp.swift
+//  UalaChallenge
+//
+//  Created by Luis Barrios on 20/3/25.
+//
+
+import SwiftUI
+
+@main
+struct UalaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UalaChallengeTests/UalaChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  UalaChallengeTests.swift
+//  UalaChallengeTests
+//
+//  Created by Luis Barrios on 20/3/25.
+//
+
+import Testing
+@testable import UalaChallenge
+
+struct UalaChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
